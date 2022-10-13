575147535a80e209bd7a5664666751ae2a2e97f3
Michael Terry <michael.terry@childrens.harvard.edu>
2022-10-13 13:48:39 -0400

    chore: delete deprecated and unused fhir template code
    Also separate shipped resources and test resources.

--- test/test_fhir_template.py
+++ test/test_fhir_template.py
@@ -3,9 +3,6 @@
 import unittest
 from fhirclient.models.patient import Patient
 from fhirclient.models.encounter import Encounter
-from fhirclient.models.observation import Observation
-from fhirclient.models.condition import Condition
-from fhirclient.models.documentreference import DocumentReference
 
 from cumulus import fhir_template
 
@@ -23,46 +20,4 @@ class TestResourcesFhirTemplates(unittest.TestCase):
         expected = fhir_template.fhir_encounter()
         actual = Encounter(expected).as_json()
 
-        self.assertDictEqual(expected, actual)
-
-    def test_observation(self):
-        expected = fhir_template.fhir_observation()
-        actual = Observation(expected).as_json()
-
-        self.assertDictEqual(expected, actual)
-
-    def ignore_test_documentreference(self):
-        """
-        Confirms fhirclient library is not able to JSON serialize
-        * indexed
-        * context/encounter
-        """
-        expected = fhir_template.fhir_documentreference()
-        actual = DocumentReference(expected, strict=False).as_json()
-
-        self.assertDictEqual(expected, actual)
-
-    def ignore_test_condition(self):
-        """
-        "OK" test fails verification because fhirclient __init__ does not
-        support
-        * clinicalStatus
-        * verificationStatus
-        in the same way as the FHIR documentation and examples.
-        """
-        expected = fhir_template.fhir_condition()
-        actual = Condition(expected).as_json()
-        self.assertDictEqual(expected, actual)
-
-    def ignore_test_condition_example(self):
-        """
-        Confirms fhirclient library is not able to JSON serialize
-        * clinicalStatus
-        * verificationStatus
-
-        https://build.fhir.org/condition-example.json.html
-        """
-        expected = fhir_template.template(
-            fhir_template.FHIRTemplate.FHIR_CONDITION_EXAMPLE)
-        actual = Condition(jsondict=expected, strict=False).as_json()
         self.assertDictEqual(expected, actual)
