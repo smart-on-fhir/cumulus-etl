0f25784691f8c5c5c32a13e9a2df65123df916f3
comorbidity <andrew.mcmurry@tch.harvard.edu>
2022-11-07 12:24:26 -0800

    cohort generated: Nov6_352am
    test_i2b2_cohort.py::test_summary_counts PASSED                          [100%]30	Congestion or runny nose
    30	Cough
    30	Diarrhea
    30	Fatigue
    30	Fever or chills
    30	Headache
    30	Muscle or body aches
    30	Nausea or vomiting
    30	Loss of taste or smell
    30	Dyspnea
    30	Sore throat
    30	X

--- test/test_i2b2_cohort.py
+++ test/test_i2b2_cohort.py
@@ -17,7 +17,7 @@ from cumulus.loaders.i2b2.cohort import CohortSelection
 from cumulus.loaders.i2b2 import extract, transform
 from cumulus.loaders.i2b2.schema import PatientDimension, VisitDimension, ObservationFact
 
-LABEL_STUDIO_NER = """
+LABELS_LIST = """
 Fever or chills
 Cough
 Nausea or vomiting
@@ -28,8 +28,10 @@ Headache
 Fatigue
 Dyspnea
 Muscle or body aches
-Anosmia
-"""
+Loss of taste or smell
+X
+""".strip().splitlines()
+
 def clean_text(physician_note: str) -> str:
     """
     Clean (replace) noisy chars like '¿' from physician note.
@@ -59,40 +61,119 @@ def cache_ctakes(physician_note: str) -> CtakesJSON:
 
 def dir_folder(path: str) -> str:
     """
-    Replace with fs-spec
+    TODO: Replace with fs-spec
     """
     filepart = path.split('/')[-1]
     folder = path.replace(filepart, '')
     os.makedirs(folder, exist_ok=True)
     return folder
 
-def dir_cohort(symptom: str, cui: str) -> str:
+def dir_cohort(subdir=None) -> str:
     """
-    @param cui: Concept Unique ID
-    @param pref: Symptom Preferred Name (like: "Fever or Chills")
+    @param subdir: Symptom Preferred Name (like: "Fever or Chills")
     @return: path to dir (dynamically created dir cohort/FeverOrChills/C0085593)
     """
-    return dir_folder(f"cohort/{symptom.title().replace(' ','')}/{cui}/")
+    if subdir:
+        return dir_folder(f"cohort/{subdir.title().replace(' ', '')}")
+    else:
+        return dir_folder(f'cohort/')
 
-def save_labelstudio(labelstudio: LabelStudio, symptom: str, cui: str, filename: str) -> str:
+def save_labelstudio(labelstudio: LabelStudio, note_id: str) -> str:
     """
     @param labelstudio: ChartReview file containing physician note and parsed JSON in LabelStudio format.
-    @param symptom: "label" in LabelStudio
+    @param subdir: subdirectory for files.
     @param cui: concept unique ID, not strictly needed but useful for keeping track.
-    @param filename: where to save labelstudio file.
+    @param note_id: where to save labelstudio file.
     @return: path to labelstudio JSON file
     """
     labelstudio.load_lazy()
 
-    cohort_dir = dir_cohort(symptom, cui)
-    path = f'{cohort_dir}/{filename}'
+    #tstmp = common.timestamp_filename()
+    path = f'{dir_cohort()}/{note_id}.json'
 
     common.write_json(path, labelstudio.as_json())
-
     return path
 
-#class TestCohortCovidSymptoms(unittest.TestCase):
-class TestCohortCovidSymptoms:
+def get_chart_labels(population: dict) -> dict:
+    out = dict()
+
+    for label, chart_list in population.items():
+        for chart in set(chart_list):
+            if chart not in out.keys():
+                out[chart] = list()
+            out[chart].append(label)
+    return out
+
+def tabulate(population: dict, chart_seq: list) -> str:
+    # header row
+    out = ['labels:\t'] + [f"{label}\t" for label in LABELS_LIST]
+
+    chart_label = get_chart_labels(population)
+
+    for chart in chart_seq:
+        out.append(f'\n{chart}')
+        for label in LABELS_LIST:
+            if label in chart_label[chart]:
+                out.append('\t1')
+            else:
+                out.append('\t0')
+    return ''.join(out)
+
+def merge_populations(population1, population2) -> dict:
+    """
+    @param population1:
+    @param population2:
+    @return:
+    """
+    if not isinstance(population1, dict):
+        population1 = common.read_json(population1)
+    if not isinstance(population2, dict):
+        population2 = common.read_json(population2)
+
+    charts1 = get_chart_labels(population1)
+    charts2 = get_chart_labels(population2)
+
+    print(f'#charts1 - {len(charts1.keys())}')
+    print(f'#charts2 = {len(charts2.keys())}')
+
+    for chart in charts2.keys():
+        if chart not in charts1.keys():
+            charts1[chart] = charts2[chart]
+
+    print(f'#merged - {len(charts1.keys())}')
+
+    return charts1
+
+def score_kappa(ann1: list, ann2: list):
+    """
+    Computes Cohen kappa for pair-wise annotators.
+    https://gist.github.com/LouisdeBruijn/1db0283dc69916516e2948f0eefc3a6e#file-cohen_kappa-py
+
+    :param ann1: annotations provided by first annotator
+    :type ann1: list
+    :param ann2: annotations provided by second annotator
+    :type ann2: list
+    :rtype: float
+    :return: Cohen kappa statistic
+    """
+    count = 0
+    for an1, an2 in zip(ann1, ann2):
+        if an1 == an2:
+            count += 1
+    A = count / len(ann1)  # observed agreement A (Po)
+
+    uniq = set(ann1 + ann2)
+    E = 0  # expected agreement E (Pe)
+    for item in uniq:
+        cnt1 = ann1.count(item)
+        cnt2 = ann2.count(item)
+        count = ((cnt1 / len(ann1)) * (cnt2 / len(ann2)))
+        E += count
+
+    return round((A - E) / (1 - E), 4)
+
+
+class TestCohortCovidSymptoms(unittest.TestCase):
     """Test case for cohort """
 
     def test_criteria(self):
@@ -103,7 +184,7 @@ class TestCohortCovidSymptoms:
         obs.start_date = parse_fhir_date('2020-03-01')
         obs.end_date = parse_fhir_date('2023-01-01')
         obs.concept_cd = map_cui_pref(covid_symptoms())
-        obs.concept_cd['X'] = 'X' # No Symptoms of COVID
+        obs.concept_cd['X'] = 'X'  # No Symptoms of COVID
 
         patient = PatientDimension()
         # patient.birth_date = fhir_common.parse_fhir_period('1992-01-01', '2023-01-01')
@@ -119,7 +200,7 @@ class TestCohortCovidSymptoms:
 
         return select
 
-    def test_select_notes(self, notes_csv, cnt_notes=20):
+    def test_select_notes(self, notes_csv, cnt_notes=30):
 
         criteria = self.test_criteria()
         criteria_cui_symptom = criteria.observation.concept_cd
@@ -133,6 +214,7 @@ class TestCohortCovidSymptoms:
         random.shuffle(obsfact_list)
 
         seen = list()
+        selected = list()
         population = {}
 
         for cui, symptom_pref in criteria_cui_symptom.items():
@@ -163,33 +245,42 @@ class TestCohortCovidSymptoms:
                 filter_cui=criteria_cui_symptom)
 
             # SelectChart: ** no ** 0 Symptoms of COVID
-            criteria_sx_no = (not nlp_cui_matches) and (len(population['X']) < cnt_notes)
+            criteria_sx_no = (not nlp_cui_matches) and (len(set(population['X'])) < cnt_notes)
 
             if criteria_sx_no:
-                labelstudio_file = save_labelstudio(
-                    labelstudio,
-                    'No Symptoms Of Covid', 'X',
-                    f'no_sx_obsfact_{obsfact.instance_num}.json')
-
-                population['X'].append(labelstudio_file)
+                selected.append(obsfact.instance_num)
+                population['X'].append(obsfact.instance_num)
+                save_labelstudio(labelstudio, obsfact.instance_num)
 
             # SelectChart: ** yes ** 1+ Symptoms of COVID
             elif nlp_cui_matches:
 
                 for cui, symptom_pref in criteria_cui_symptom.items():
-                    criteria_sx_yes = (cui in nlp_cui_matches) and (len(population[symptom_pref]) < cnt_notes)
+                    criteria_sx_yes = (cui in nlp_cui_matches) and (len(set(population[symptom_pref])) < cnt_notes)
 
                     if criteria_sx_yes:
+                        save_labelstudio(labelstudio, obsfact.instance_num)
+
+                        population[symptom_pref].append(obsfact.instance_num)
+                        common.write_json('cohort/population.json', population)
+                        common.write_text('cohort/tabulate.tsv', tabulate(population, chart_seq=selected))
+
+                        if obsfact.instance_num not in selected:
+                            selected.append(obsfact.instance_num)
+
                         print(f'{cui} in {nlp_cui_matches} {symptom_pref}')
-
-                        labelstudio_file = save_labelstudio(
-                            labelstudio,
-                            symptom_pref, cui,
-                            f'cui_{cui}_obsfact_{obsfact.instance_num}.json')
-
-                        population[symptom_pref].append(labelstudio_file)
             else:
                 print(f'no match, len(seen) = {len(seen)}')
+
+def test_merge_population():
+    pop1 = '/Users/andy/chip/cumulus-etl/cohort_Nov5_100/population.json'
+    pop2 = '/Users/andy/chip/cumulus-etl/cohort_Nov5_1051am/population.json'
+    merged = merge_populations(pop1, pop2)
+    print(merged)
+
+def test_summary_counts():
+    for label, chart_list in common.read_json('/Users/andy/chip/cumulus-etl/cohort/population.json').items():
+        print(f'{len(set(chart_list))}\t{label}')
 
 
 if __name__ == '__main__':
