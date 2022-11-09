6e4db0fe087dfc7a8fe01eecf630bc2308eb6dc1
Michael Terry <michael.terry@childrens.harvard.edu>
2022-11-09 09:23:46 -0500

    Minor cleanups around ndjson loading
    - Move loaders/fhir_ndjson.py into its own directory (loaders/fhir/)
    - Add some convenience code for printing console output headers
    - Support a wider naming scheme for ndjson files (i.e. no longer
      require exactly Condition.ndjson, but allow multiple Condition
      files, named like Condition.1.ndjson or similar)
    - Fix a bug where we assumed document references would have a
      context list, which is optional per the spec

--- cumulus/loaders/__init__.py
+++ cumulus/loaders/__init__.py
@@ -1,5 +1,5 @@
 """Public API for loaders"""
 
 from .base import Loader
-from .fhir_ndjson import FhirNdjsonLoader
+from .fhir.fhir_ndjson import FhirNdjsonLoader
 from .i2b2.loader import I2b2Loader
--- cumulus/loaders/fhir/fhir_ndjson.py
+++ cumulus/loaders/fhir/fhir_ndjson.py
@@ -2,24 +2,26 @@
 
 import tempfile
 
-from cumulus.loaders.base import Loader
+from cumulus.loaders import base
 
 
-class FhirNdjsonLoader(Loader):
+class FhirNdjsonLoader(base.Loader):
     """
     Loader for fhir ndjson data.
 
-    Expected format is a folder with one ndjson files per category (i.e. Condition.ndjson)
-    TODO: make this a little more flexible, once we know what we want here
+    Expected local-folder format is a folder with ndjson files labeled by resource type.
+    (i.e. Condition.000.ndjson or Condition.ndjson)
     """
 
     def load_all(self) -> tempfile.TemporaryDirectory:
+        # Are we reading from a local directory?
         if self.root.protocol == 'file':
             # We can actually just re-use the input dir without copying the files, since everything is local.
             class Dir:
                 name: str = self.root.path
             return Dir()  # once we drop python3.7, we can have load_all return a Protocol for proper typing
 
+        # Fall back to copying from a remote directory (like S3 buckets) to a local one
         tmpdir = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
         self.root.get(self.root.joinpath('*.ndjson'), f'{tmpdir.name}/')
         return tmpdir
