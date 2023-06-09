ff184c7a62bd62e19ac61ea0ca6e9370b88c9dcc
Michael Terry <michael.terry@childrens.harvard.edu>
2023-06-09 15:32:15 -0400

    refactor: move all covid_symptom code into its own submodule
    This isolates the study-specific code and gives an example for future
    studies / further covid code.

--- cumulus_etl/etl/tasks.py
+++ cumulus_etl/etl/tasks.py
@@ -1,15 +1,12 @@
 """ETL tasks"""
 
-import copy
-import itertools
 import os
 import sys
 from typing import AsyncIterable, AsyncIterator, Iterable, Iterator, List, Set, Type, TypeVar, Union
 
-import ctakesclient
 import pandas
 
-from cumulus_etl import common, deid, errors, nlp, store
+from cumulus_etl import common, deid, errors, store
 from cumulus_etl.etl import config
 
 T = TypeVar("T")
@@ -125,6 +122,10 @@ class EtlTask:
 
         :returns: a list of all EtlTask subclasses, to instantiate and run
         """
+        # Nested imports to avoid circular dependencies
+        # pylint: disable=import-outside-toplevel
+        from cumulus_etl.etl.studies import covid_symptom
+
         # Right now, just hard-code these. One day we might allow plugins or something similarly dynamic.
         return [
             ConditionTask,
@@ -133,7 +134,7 @@ class EtlTask:
             MedicationRequestTask,
             ObservationTask,
             PatientTask,
-            CovidSymptomNlpResultsTask,
+            covid_symptom.CovidSymptomNlpResultsTask,
         ]
 
     @classmethod
@@ -345,107 +346,3 @@ class PatientTask(EtlTask):
     name = "patient"
     resource = "Patient"
     tags = {"cpu"}
-
-
-class CovidSymptomNlpResultsTask(EtlTask):
-    """Covid Symptom study task, to generate symptom lists from ED notes using NLP"""
-
-    name = "covid_symptom__nlp_results"
-    resource = "DocumentReference"
-    tags = {"covid_symptom", "gpu"}
-    group_field = "docref_id"
-
-    # List of recognized emergency department note types. We'll add more as we discover them in use.
-    ED_CODES = {
-        "http://cumulus.smarthealthit.org/i2b2": {
-            "NOTE:3710480",
-            "NOTE:3807712",
-            "NOTE:149798455",
-            "NOTE:159552404",
-            "NOTE:189094576",
-            "NOTE:189094619",
-            "NOTE:189094644",
-            "NOTE:318198107",
-            "NOTE:318198110",
-            "NOTE:318198113",
-        },
-        "http://loinc.org": {
-            "18842-5",  # Discharge Summary
-            "28568-4",  # Physician Emergency department Note
-            "34111-5",  # Emergency department Note
-            "34878-9",  # Emergency medicine Note
-            "51846-4",  # Emergency department Consult note
-            "54094-8",  # Emergency department Triage note
-            "57053-1",  # Nurse Emergency department Note
-            "57054-9",  # Nurse Emergency department Triage+care note
-            "59258-4",  # Emergency department Discharge summary
-            "60280-5",  # Emergency department Discharge instructions
-            "68552-9",  # Emergency medicine Emergency department Admission evaluation note
-            "74187-6",  # InterRAI Emergency Screener for Psychiatry (ESP) Document
-            "74211-4",  # Summary of episode note Emergency department+Hospital
-        },
-    }
-
-    async def prepare_task(self) -> bool:
-        bsv_path = ctakesclient.filesystem.covid_symptoms_path()
-        success = nlp.restart_ctakes_with_bsv(self.task_config.ctakes_overrides, bsv_path)
-        if not success:
-            print(f"Skipping {self.name}.")
-        return success
-
-    @classmethod
-    def is_ed_coding(cls, coding):
-        """Returns true if this is a coding for an emergency department note"""
-        return coding.get("code") in cls.ED_CODES.get(coding.get("system"), {})
-
-    def add_error(self, docref: dict) -> None:
-        if not self.task_config.dir_errors:
-            return
-
-        error_root = store.Root(os.path.join(self.task_config.dir_errors, self.name), create=True)
-        error_path = error_root.joinpath("nlp-errors.ndjson")
-        with common.NdjsonWriter(error_path, "a") as writer:
-            writer.write(docref)
-
-    async def read_entries(self) -> AsyncIterator[Union[List[dict], dict]]:
-        """Passes clinical notes through NLP and returns any symptoms found"""
-        phi_root = store.Root(self.task_config.dir_phi, create=True)
-
-        # one client for both NLP services for now -- no parallel requests yet, so no need to be fancy
-        http_client = nlp.ctakes_httpx_client()
-
-        for docref in self.read_ndjson():
-            # Only bother running NLP on docs that are current & finalized
-            bad_ref_status = docref.get("status") in ("superseded", "entered-in-error")  # status of DocRef itself
-            bad_doc_status = docref.get("docStatus") in ("preliminary", "entered-in-error")  # status of clinical note
-            if bad_ref_status or bad_doc_status:
-                continue
-
-            # Check that the note is one of our special allow-listed types (we do this here rather than on the output
-            # side to save needing to run everything through NLP).
-            # We check both type and category for safety -- we aren't sure yet how EHRs are using these fields.
-            codings = list(itertools.chain.from_iterable([cat.get("coding", []) for cat in docref.get("category", [])]))
-            codings += docref.get("type", {}).get("coding", [])
-            is_er_note = any(self.is_ed_coding(x) for x in codings)
-            if not is_er_note:
-                continue
-
-            orig_docref = copy.deepcopy(docref)
-            if not self.scrubber.scrub_resource(docref, scrub_attachments=False):
-                continue
-
-            symptoms = await nlp.covid_symptoms_extract(
-                self.task_config.client,
-                phi_root,
-                docref,
-                ctakes_http_client=http_client,
-                cnlp_http_client=http_client,
-            )
-            if symptoms is None:
-                self.add_error(orig_docref)
-                continue
-
-            # Yield the whole set of symptoms at once, to allow for more easily replacing previous a set of symptoms.
-            # This way we don't need to worry about symptoms from the same note crossing batch boundaries.
-            # The Format class will replace all existing symptoms from this note at once (because we set group_field).
-            yield symptoms
--- cumulus_etl/nlp/extract.py
+++ cumulus_etl/nlp/extract.py
@@ -1,14 +1,13 @@
 """Interface for talking to a cTAKES server"""
 
 import hashlib
-import logging
 import os
-from typing import List, Optional
+from typing import List
 
 import ctakesclient
 import httpx
 
-from cumulus_etl import common, fhir_client, fhir_common, store
+from cumulus_etl import common, store
 
 
 def ctakes_httpx_client() -> httpx.AsyncClient:
@@ -16,93 +15,7 @@ def ctakes_httpx_client() -> httpx.AsyncClient:
     return httpx.AsyncClient(timeout=timeout)
 
 
-async def covid_symptoms_extract(
-    client: fhir_client.FhirClient,
-    cache: store.Root,
-    docref: dict,
-    ctakes_http_client: httpx.AsyncClient = None,
-    cnlp_http_client: httpx.AsyncClient = None,
-) -> Optional[List[dict]]:
-    """
-    Extract a list of Observations from NLP-detected symptoms in clinical notes
-
-    :param client: a client ready to talk to a FHIR server
-    :param cache: Where to cache NLP results
-    :param docref: Clinical Note
-    :param ctakes_http_client: HTTPX client to use for the cTAKES server
-    :param cnlp_http_client: HTTPX client to use for the cNLP transformer server
-    :return: list of NLP results encoded as FHIR observations
-    """
-    docref_id = docref["id"]
-    _, subject_id = fhir_common.unref_resource(docref["subject"])
-
-    encounters = docref.get("context", {}).get("encounter", [])
-    if not encounters:
-        logging.warning("No encounters for docref %s", docref_id)
-        return None
-    _, encounter_id = fhir_common.unref_resource(encounters[0])
-
-    # Find the clinical note among the attachments
-    try:
-        clinical_note = await fhir_common.get_docref_note(client, docref)
-    except Exception as exc:  # pylint: disable=broad-except
-        logging.warning("Error getting text for docref %s: %s", docref_id, exc)
-        return None
-
-    # cTAKES cache namespace history (and thus, cache invalidation history):
-    #   v1: original cTAKES processing
-    # TODO: Ideally we'd also be able to ask ctakesclient for NLP algorithm information as part of this namespace.
-    #  For now, we'll manually update this namespace if/when the cTAKES algorithm we use changes.
-    ctakes_namespace = "covid_symptom_v1"
-
-    # cNLP cache namespace history (and thus, cache invalidation history):
-    #   v1: original addition of cNLP filtering
-    #   v2: we started dropping non-covid symptoms, which changes the span ordering
-    cnlp_namespace = f"{ctakes_namespace}-cnlp_v2"
-
-    try:
-        ctakes_json = await extract(cache, ctakes_namespace, clinical_note, client=ctakes_http_client)
-    except Exception as exc:  # pylint: disable=broad-except
-        logging.warning("Could not extract symptoms for docref %s (%s): %s", docref_id, type(exc).__name__, exc)
-        return None
-
-    matches = ctakes_json.list_sign_symptom(ctakesclient.typesystem.Polarity.pos)
-
-    # Filter out any match that isn't a covid symptom, for speed of NLP & Athena and to keep minimal data around
-    covid_symptom_cuis = {s.cui for s in ctakesclient.filesystem.covid_symptoms()}
-
-    def is_covid_match(m: ctakesclient.typesystem.MatchText):
-        return bool(covid_symptom_cuis.intersection({attr.cui for attr in m.conceptAttributes}))
-
-    matches = list(filter(is_covid_match, matches))
-
-    # OK we have cTAKES symptoms. But let's also filter through cNLP transformers to remove any that are negated
-    # there too. We have found this to yield better results than cTAKES alone.
-    try:
-        spans = ctakes_json.list_spans(matches)
-        polarities_cnlp = await list_polarity(cache, cnlp_namespace, clinical_note, spans, client=cnlp_http_client)
-    except Exception as exc:  # pylint: disable=broad-except
-        logging.warning("Could not check negation for docref %s (%s): %s", docref_id, type(exc).__name__, exc)
-        return None
-
-    # Now filter out any non-positive matches
-    positive_matches = []
-    for i, match in enumerate(matches):
-        if polarities_cnlp[i] == ctakesclient.typesystem.Polarity.pos:
-            positive_matches.append(
-                {
-                    "id": f"{docref_id}.{i}",
-                    "docref_id": docref_id,
-                    "encounter_id": encounter_id,
-                    "subject_id": subject_id,
-                    "match": match.as_json(),
-                }
-            )
-
-    return positive_matches
-
-
-async def extract(
+async def ctakes_extract(
     cache: store.Root, namespace: str, sentence: str, client: httpx.AsyncClient = None
 ) -> ctakesclient.typesystem.CtakesJSON:
     """
