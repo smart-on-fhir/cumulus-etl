d386b810a8f8d72050d06fb7e7370066db5f45b7
Dylan Phelan <Dtphelan1@gmail.com>
2025-12-18 16:29:01 -0500

    Firse pass at porting over pydantic classes and creating tasks

---
+++ cumulus_etl/etl/studies/glioma/__init__.py
@@ -0,0 +1,42 @@
+"""The glioma study"""
+
+from .irae_tasks import IraeDonorClaudeSonnet45Task as IraeDonorClaudeSonnet45Task
+from .irae_tasks import IraeDonorGpt4oTask as IraeDonorGpt4oTask
+from .irae_tasks import IraeDonorGpt5Task as IraeDonorGpt5Task
+from .irae_tasks import IraeDonorGptOss120bTask as IraeDonorGptOss120bTask
+from .irae_tasks import IraeDonorLlama4ScoutTask as IraeDonorLlama4ScoutTask
+from .irae_tasks import (
+    IraeImmunosuppressiveMedicationsClaudeSonnet45Task as IraeImmunosuppressiveMedicationsClaudeSonnet45Task,
+)
+from .irae_tasks import (
+    IraeImmunosuppressiveMedicationsGpt4oTask as IraeImmunosuppressiveMedicationsGpt4oTask,
+)
+from .irae_tasks import (
+    IraeImmunosuppressiveMedicationsGpt5Task as IraeImmunosuppressiveMedicationsGpt5Task,
+)
+from .irae_tasks import (
+    IraeImmunosuppressiveMedicationsGptOss120bTask as IraeImmunosuppressiveMedicationsGptOss120bTask,
+)
+from .irae_tasks import (
+    IraeImmunosuppressiveMedicationsLlama4ScoutTask as IraeImmunosuppressiveMedicationsLlama4ScoutTask,
+)
+from .irae_tasks import IraeLongitudinalClaudeSonnet45Task as IraeLongitudinalClaudeSonnet45Task
+from .irae_tasks import IraeLongitudinalGpt4oTask as IraeLongitudinalGpt4oTask
+from .irae_tasks import IraeLongitudinalGpt5Task as IraeLongitudinalGpt5Task
+from .irae_tasks import IraeLongitudinalGptOss120bTask as IraeLongitudinalGptOss120bTask
+from .irae_tasks import IraeLongitudinalLlama4ScoutTask as IraeLongitudinalLlama4ScoutTask
+from .irae_tasks import (
+    IraeMultipleTransplantHistoryClaudeSonnet45Task as IraeMultipleTransplantHistoryClaudeSonnet45Task,
+)
+from .irae_tasks import (
+    IraeMultipleTransplantHistoryGpt4oTask as IraeMultipleTransplantHistoryGpt4oTask,
+)
+from .irae_tasks import (
+    IraeMultipleTransplantHistoryGpt5Task as IraeMultipleTransplantHistoryGpt5Task,
+)
+from .irae_tasks import (
+    IraeMultipleTransplantHistoryGptOss120bTask as IraeMultipleTransplantHistoryGptOss120bTask,
+)
+from .irae_tasks import (
+    IraeMultipleTransplantHistoryLlama4ScoutTask as IraeMultipleTransplantHistoryLlama4ScoutTask,
+)
