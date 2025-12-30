cec0cff762b7291e998b7c57e8f848bcc4f03120
Dylan Phelan <Dtphelan1@gmail.com>
2025-12-30 15:21:02 -0500

    removed the unused ingredient code

--- tests/glioma/test_glioma_med_models.py
+++ tests/glioma/test_glioma_med_models.py
@@ -2,7 +2,7 @@
 
 import ddt
 
-from cumulus_etl.etl.studies.glioma.glioma_med_models import drug_type_field, ingredient_field
+from cumulus_etl.etl.studies.glioma.glioma_med_models import drug_type_field
 from tests import utils
 
 
@@ -26,17 +26,3 @@ class TestGliomaMedModelHelpers(utils.AsyncTestCase):
             "class or therapy modality documented for this medication, if present",
             field.description,
         )
-
-    @ddt.data(
-        (None, "Temozolomide"),
-        ("some_default", "Temozolomide"),
-        ("another_default", "Carmustine"),
-    )
-    @ddt.unpack
-    def ingredient_field(self, default, ingredient):
-        """Test the ingredient_field helper function."""
-        field = ingredient_field(default=default, ingredient=ingredient)
-        self.assertEqual(field.default, default)
-        self.assertIn(ingredient, field.description)
-        self.assertIn("Extract the", field.description)
-        self.assertIn("ingredient documented for this medication, if present", field.description)
