75557176f93a9c4a7cbd27bf31fcb43f1fdecbba
Dylan Phelan <Dtphelan1@gmail.com>
2025-12-30 14:47:57 -0500

    added glioma test for medmodel helpers

---
+++ tests/glioma/test_glioma_med_models.py
@@ -0,0 +1,56 @@
+"""Tests for Glioma medication models helpers"""
+
+import ddt
+
+from cumulus_etl.etl.studies import covid_symptom
+from cumulus_etl.etl.studies.glioma.glioma_med_models import ingredient_field, drug_type_field
+from tests import utils
+
+# def drug_type_field(default=None, drug_type=None) -> str | None:
+#     return Field(
+#         default=default,
+#         description=f"Extract the {drug_type} class or therapy modality documented for this medication, if present",
+#     )
+
+
+# def ingredient_field(default=None, ingredient=None) -> str | None:
+#     return Field(
+#         default=default,
+#         description=f"Extract the {ingredient} ingredient documented for this medication, if present",
+#     )
+
+
+@ddt.ddt
+class TestGliomaMedModelHelpers(utils.AsyncTestCase):
+    """Test case for glioma's medication model helpers."""
+
+    @ddt.data(
+        (None, "chemotherapy"),
+        ("some_default", "chemotherapy"),
+        ("another_default", "another cancer drug type"),
+    )
+    @ddt.unpack
+    def test_drug_type_field(self, default, drug_type):
+        """Test the drug_type_field helper function."""
+        field = drug_type_field(default=default, drug_type=drug_type)
+        self.assertEqual(field.default, default)
+        self.assertIn(drug_type, field.description)
+        self.assertIn("Extract the", field.description)
+        self.assertIn(
+            "class or therapy modality documented for this medication, if present",
+            field.description,
+        )
+
+    @ddt.data(
+        (None, "Temozolomide"),
+        ("some_default", "Temozolomide"),
+        ("another_default", "Carmustine"),
+    )
+    @ddt.unpack
+    def ingredient_field(self, default, ingredient):
+        """Test the ingredient_field helper function."""
+        field = ingredient_field(default=default, ingredient=ingredient)
+        self.assertEqual(field.default, default)
+        self.assertIn(ingredient, field.description)
+        self.assertIn("Extract the", field.description)
+        self.assertIn("ingredient documented for this medication, if present", field.description)
