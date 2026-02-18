2d4b32289093bf92c91937044a6682c0964ceed6
Michael Terry <michael.terry@childrens.harvard.edu>
2026-02-18 14:10:26 -0500

    Drop support for extracting i2b2 from an Oracle server
    In keeping with dropping bulk export support, we don't want the ETL to
    be in the business of extracting data itself. And this lets us remove
    some really crufty code and a dependency.

--- tests/loaders/i2b2/test_i2b2_etl.py
+++ tests/loaders/i2b2/test_i2b2_etl.py
@@ -29,14 +29,3 @@ class TestI2b2Etl(BaseEtlSimple):
             ],
         )
         self.assert_output_equal()
-
-    async def test_export(self):
-        with tempfile.TemporaryDirectory() as export_path:
-            # Only run patient task to make the test faster and confirm we don't export unnecessary files
-            await self.run_etl(
-                input_format="i2b2", export_to=export_path, tasks=["patient"], philter=False
-            )
-
-            expected_export_path = os.path.join(self.datadir, self.DATA_ROOT, "export")
-            dircmp = filecmp.dircmp(export_path, expected_export_path)
-            self.assert_file_tree_equal(dircmp)
